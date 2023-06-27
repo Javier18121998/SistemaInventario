a832b60774e154cce3808b8566c4c7b25d780be4
JavierMarinGarcia <javier.marin.garcia.linkedin01@hotmail.com>
2023-06-27 09:10:54 -0600

    Patron de Repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/BodegaRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class BodegaRepositorio : Repositorio<Bodega>, IBodegaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+        public BodegaRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Acutaliza(Bodega bodega)
+        {
+            var bodegaDB = _db.Bodegas.FirstOrDefault(b => b.Id == bodega.Id);
+            if (bodegaDB != null) 
+            {
+                bodegaDB.Nombre = bodega.Nombre;
+                bodegaDB.Descripción = bodega.Descripción;
+                bodegaDB.Estado = bodega.Estado;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
